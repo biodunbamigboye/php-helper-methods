75df8a7e89dfbd10f8a528f754db995581d3a24c
Moffat Munene <moffmu@gmail.com>
2023-07-26 23:58:02 +0300

    initial setup

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+
+use Biodun\PhpHelperMethods\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
